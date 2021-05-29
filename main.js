8b857c60345bb42b0387a6bcfeecc16896a67226
Varsha1018 <76804038+Varsha1018@users.noreply.github.com>
2021-05-29 15:06:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:"png",
+    png_quality:100
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='picture' src='"+data_uri+"'>";
+    });
+}
+console.log("ml5 version:",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tYXQRXNrr/model.json',modellodded);
+
+function modellodded(){
+    console.log("model lodded");
+}
+
+function check(){
+    img=document.getElementById("picture");
+    classifier.classify(img,result);
+}
+
+function result(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
+
